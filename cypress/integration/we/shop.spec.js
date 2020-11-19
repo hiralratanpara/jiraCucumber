c20b392967b8233a9adbdbf4520cb616eacd4664
Indramohan Rathore <irathore@wsgc.com>
2020-11-19 12:36:42 -0500

    Test Cases for Shop Page

--- cypress/integration/we/shop.spec.js
+++ cypress/integration/we/shop.spec.js
@@ -1,22 +1,47 @@
 /// <reference types='Cypress'/>
 import { data } from "../../fixtures/we/test-data.json";
+import {searchPage,productPage,pipPage} from '../../locators/we/pip.json'
+import {category} from '../../locators/we/shop.json'
 describe('WE Test Shop Page ', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
       });
-
-      it('should display visual navigation', () => {
+  
+    before(() => {
         cy.visit(data.brandURL);
-            cy.get('.category-6 > .c-nav-category_link > span').click();
-            cy.get('.left-nav > :nth-child(2) > :nth-child(3) > a').click();
-            cy.wait(5000);
-            cy.get('body').find( '#join-email-campaign > .shader > .modal-scroll > .modal_component > .btnClose' ).its('length').then(res=>{
-                if(res > 0){
-                    cy.get('#join-email-campaign > .shader > .modal-scroll > .modal_component > .btnClose').click();
-                
-                }
-            });
-           cy.get('.shop-hero-1').should('be.visible');
-       
+        cy.wait(5000);
+        cy.get('body').find( '.stickyOverlayCloseButton' ).its('length').then(res=>{
+            if(res > 0){
+                cy.get('.stickyOverlayCloseButton').click();
+            
+            }
         });
+        
+    })
+    it('Should search SKU', () => {
+      cy.get(searchPage.searchBox).type(data.homePageData.skuNo).submit()
+    })
+
+    it('Should load the sku', () => {
+       cy.get(pipPage.productLabel).get('h1').contains(data.homePageData.skuName)
+    })
+
+    it('Should display the correct swatch', () => {
+       cy.get(pipPage.swatchLabel).should('have.text', data.homePageData.defaultSwatch);
+    })
+
+    it('should display visual navigation', () => {
+        cy.get(category.superCat).click();
+        cy.get(category.leftMenu).click();
+        
+        cy.get('body').find( '#join-email-campaign > .shader > .modal-scroll > .modal_component > .btnClose' ).its('length').then(res=>{
+            if(res > 0){
+                cy.get('#join-email-campaign > .shader > .modal-scroll > .modal_component > .btnClose').click();
+            
+            }
+        });
+        cy.wait(5000);
+        cy.get(category.visualNav).should('be.visible');
+    
     });
+});
